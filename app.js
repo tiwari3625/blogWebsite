e2753f2d476f094e7b288a32e31abbc69bd3ce17
Shiv <tshiv3625@gmail.com>
2020-07-12 22:15:00 +0530

    Final Complete

--- app.js
+++ app.js
@@ -76,7 +76,11 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
